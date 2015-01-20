02cf1576054006431fdb3c94a369032054956b49
stian <s.3_dokken@hotmail.com>
2015-01-20 22:06:53 +0100

    go kode med mutex. Fikk ikke til med channels. Fungerer bra med mutex

--- Exercise2/helloworld_go_fixed.go
+++ Exercise2/helloworld_go_fixed.go
@@ -1,6 +1,5 @@
 // Go 1.2
 // go run helloworld_go.go
-
 package main
 
 import (
@@ -8,27 +7,33 @@ import (
                 //   This is probably not a good idea for large projects...
     "runtime"
     "time"
+    "sync"
 )
 
-var i int = 0;  
+var i int = 0
+var mutex = &sync.Mutex{}
 
 func goroutine_1() {
-    for n := 0;n<1000000;n++{
-		i++;
-	}
+    for j:=0;j<1000001;j++ {
+        mutex.Lock()
+        i++
+        mutex.Unlock()
+    }
 }
 
 func goroutine_2() {
-    for n := 0;n<1000000;n++{
-		i--;
-	}
+    for n:=0;n<1000001;n++ {
+        mutex.Lock()
+        i--
+        mutex.Unlock()
+    }
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())          
-    go goroutine_1() 
-	time.Sleep(100*time.Millisecond)                     
-	go goroutine_2()
+    
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    go goroutine_1()                    
+    go goroutine_2()
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     time.Sleep(100*time.Millisecond)
     Println(i)
