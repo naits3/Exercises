630c3c6ae23c97a0f6a5a79223d6260cee699971
martiskj <martiskj@stud.ntnu.no>
2015-01-23 11:47:40 +0100

    Finally fixed Go channels!

--- Exercise2/helloworld_go_fixed_channels.go
+++ Exercise2/helloworld_go_fixed_channels.go
@@ -4,34 +4,39 @@ import (
     . "fmt"     // Using '.' to avoid prefixing functions with their package names
                 //   This is probably not a good idea for large projects...
     "runtime"
-    
 )
 
-var i int = 0; 
-var c chan int 
+var i int = 0;  
 
-func goroutine_1() {
+func goroutine_1(c chan int, done chan bool) {
     for n := 0;n<1000001;n++{
-		i := <- c
-        c <- (i+1)
+		i = <- c
+        i++
+        c <- i
 	}
+    done <- true
 }
 
-func goroutine_2() {
-    for n := 0;n<1000000;n++{
-		i := <- c
-        c <- (i-1)
+func goroutine_2(c chan int, done chan bool) {
+    for f := 0;f<1000000;f++{
+		i = <- c
+        i--
+        c <- i
 	}
+    done <- true
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())   
     
-    c := make(chan int)
-    c <- 0
+    c := make(chan int,1)
+    done := make(chan bool)
     
-    go goroutine_1()               
-	go goroutine_2()
+    c <- 0
+    go goroutine_1(c, done)               
+	go goroutine_2(c, done)
 
+    <- done
+    <- done
     Println(<-c)
 }
