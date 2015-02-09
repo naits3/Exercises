82613b95865809bc58f25f03a5d0661495aadc8e
naits3 <stianss@stud.ntnu.no>
2015-02-09 14:01:48 +0100

    Sudetion to new network setup

---
+++ Exercise4/CommunicationControl.go
@@ -0,0 +1,57 @@
+package network
+
+import (
+	"fmt"
+	"net"
+)
+
+func initCommunicationControl(){
+	//initaliserer all channels(receive chan, chsend
+	//starts communication control
+	//starts also connectionControll
+}
+
+func runCommunicationControl(){
+	//Select which controls sending and receiving
+}
+
+func receivePack(port string, chReceive chan []byte){
+	listener, err := net.Listen("tcp", ":"+port)
+
+	if err != nil {
+		return []byte("0")
+	}
+
+	for {
+		var buffer []byte = make([]byte, 1024)
+		conn, err := listener.Accept()
+		packetSize, err := conn.Read(buffer)
+
+		if err != nil {
+			return []byte("0")
+		}
+
+		if packetSize > 0{
+			receive <- buffer[0:packetSize]
+		}
+	}
+}
+
+func SendPack(pack []byte, host string, chSend chan bool){
+	addr, _ := net.ResolveTCPAddr("tcp",host)
+	conn, err := net.DialTCP("tcp", nil, addr)
+
+	if err != nil{
+		return
+	}
+	
+	_, err = conn.Write(pack)
+	
+	if err != nil {
+		return
+	}
+
+	chSend <- true
+}
+
+
---
+++ Exercise4/connectionControl.go
@@ -0,0 +1,99 @@
+package network
+
+import (
+	"fmt"
+	"net"
+)
+
+var connectionMap map[string]net.Conn = nil
+var broadcastConnection net.Conn = nil   		//Variable for broadcast conn
+var port string = nil
+
+func InitConnectionControl(){
+	//Run connectionConntroll thread
+	//Run requestConnections once to see if there are other connections ready
+	//Starts listenForConnection from port
+}
+
+func runConnectionControl(){
+	// Select for Connection controll legges her
+	// Tar inn alle nødvendige channels for kommunikasjon
+}
+
+func requestConnections(ip string, port string, ){
+	//UDP broadcast en gang ved oppstart
+	//hvis ikke connection prøv igjen altså hvis det kommer en error
+}
+
+
+func listenForNewConnections(port int){
+	addr := net.UDPAddr{
+		Port: port,
+	}
+
+	conn, err := net.ListenUDP("udp", &addr)
+
+	if err != nil {
+		fmt.Println("Error listening to UDP: ",err)
+		return
+	}
+
+	var buffer []byte = make([]byte, 1024)
+	
+	defer conn.Close()
+	for {
+		n, address, err := conn.ReadFromUDP(buffer)
+
+		if err != nil {
+			fmt.Println("Error reading from UDP: ",err)
+			return
+		}
+
+		if address != nil && string(buffer[0:n])=="CONNECTME" {
+			// SEND OK HERE
+		}
+	}
+}
+
+func searchForBrokenConnection(){
+	//UDP broadcast that listens for broken connections in the map
+	//For each signal sent the number of replies should be equal to number of connections
+	//Var tidligere ping funksjonen
+}
+
+func addConnection(ip string){
+	//Legger til ip som hash variabel med en connection TCP
+}
+
+func deleteConnection(ip string){
+	//Fjerner en connection fra dictonary for TCP
+}
+
+func GetAllConnections() map[string]net.Conn{
+	// Sender alle TCP til kommunikasjonskontroll
+	// Må ha variable kontroll med addConn.. og deleteConn..
+}
+
+// Ikke ferdig!!!
+// Sette en egen variabel for broadcast signal??
+func getBroadcastConnection() string{
+	ifaces, _ := net.Interfaces()
+	// handle err
+	for _, i := range ifaces {
+	    addrs, _ := i.Addrs()
+	    // handle err
+	    for _, addr := range addrs {
+	        switch v:= addr.(type) {
+	        case *net.IPAddr:
+	            if(v.IP.String() != "0.0.0.0"){
+	            	return(v.IP.String())
+	        	}
+	        }
+	    }
+	}
+	return "is_offline"
+}
+
+
+
+
